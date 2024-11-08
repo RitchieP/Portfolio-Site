78b13f35be78f838cb3ecc8d41fcbfa8ab7e44e7
Ritchie <ritchiepoh@gmail.com>
2024-11-08 16:10:46 +0800

    feat: :lipstick: Build top nav bar

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,6 +11,7 @@ module.exports = {
         background: "var(--background)",
         foreground: "var(--foreground)",
         secondary: "var(--secondary)",
+        unhighlighted: "var(--unhighlighted)",
       }
     },
   },
